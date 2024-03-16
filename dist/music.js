f2580cd4a72123b58f80c833c1da2cae4b27f8a6
xiemt9 <xiemt9@gmail.com>
2024-03-16 20:35:50 +0800

    Site updated: 2024-03-16 20:35:49

---
+++ dist/music.js
@@ -0,0 +1,13 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      {
+        name:'风筝误',
+        artist:'刘珂矣',
+        url: 'http://up.mcyt.net/?down/46644.mp3',
+        cover: 'http://oeff2vktt.bkt.clouddn.com/image/96.jpg',
+      }
+    ]
+});
